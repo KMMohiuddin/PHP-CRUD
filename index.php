9bdf6e7dd01ada11ff751c58789fb0812adddd1e
Mohiuddin <mohiuddin35-2894@diu.edu.bd>
2023-03-13 03:19:46 +0600

    header

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 $conn = mysqli_connect("localhost", "root", "", "web_practice");
 if (isset($_POST["submit"])) {
   $name = $_POST["student_name"];
@@ -19,42 +18,130 @@ if (isset($_POST["submit"])) {
 
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>CURD index</title>
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 </head>
 
 <body>
-<div>
-  <div class="row justify-content-center align-items-center" style="background-color: #71C9CE;">
-    <div class="col-6" >
-      <h1 class="text-center py-2" >Smart Hall</h1>
-    </div>
-    <div class="col-6">
-      <form class="form-inline rounded-pill bg-light">
-        <div class="input-group">
-          <input class="form-control border-0" type="search" placeholder="Search..." aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary bg-white border-0 rounded-pill" type="submit"><i class="fas fa-search"></i></button>
+  <header class="py-3" style="background-color: #71C9CE;">
+    <div class="container">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-md-3">
+          <h3 class="text-center text-lg-left mb-0">CURD</h3>
+        </div>
+        <div class="col-md-6">
+          <div class="btn-group btn-group-lg" role="group" aria-label="CURD">
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#createModal">Create</button> <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary">Read</button>
+            <button type="button" class="btn btn-warning">Update</button>
+            <button type="button" class="btn btn-danger">Delete</button>
           </div>
         </div>
-      </form>
+        <div class="col-md-3">
+          <form class="form-inline">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <button class="btn btn-outline-secondary bg-white border-0 rounded-pill" type="submit"><i class="fas fa-search"></i></button>
+              </div>
+              <input class="form-control bg-white border-0 rounded-pill" type="search" placeholder="Search..." aria-label="Search">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </header>
+
+
+  <!-- Modal -->
+  <div class="modal fade bd-example-modal-lg" data-backdrop="static" id="createModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg role=" document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add New Data</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form class="" id="form" action="index.php" method="post">
+            <div class="col-auto">
+              <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">Name</div>
+                </div>
+                <input type="text" class="form-control" name="student_name" placeholder="Jon Doe">
+              </div>
+            </div>
+            <div class="row justify-content-between align-items-center">
+              <div class="col-sm-3 my-1">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">Age</div>
+                  </div>
+                  <input type="number" class="form-control" name="student_age">
+                </div>
+              </div>
+              <div class="col-sm-9 my-1">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">Email</div>
+                  </div>
+                  <input type="number" class="form-control" name="student_email" placeholder="jondoe@gmail.com">
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" name="submit" class="btn btn-primary">Save changes</button>
+          <button type="submit" class="btn btn-primary" name="submit" data-dismiss="modal">Submit</button>
+        </div>
+      </div>
     </div>
   </div>
-</div>
+
+
 
   <div class="container my-5">
-    <form class="" action="index.php" method="post">
-      name <input type="text" name="student_name" value="">
-      Age <input type="number" name="student_age" value="">
-      Email <input type="email" name="student_email" value="">
-      <input type="submit" name="submit" value="submit">
-    </form>
+  <form class="" id="form" action="index.php" method="post">
+            <div class="col-auto">
+              <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">Name</div>
+                </div>
+                <input type="text" class="form-control" name="student_name" placeholder="Jon Doe">
+              </div>
+            </div>
+            <div class="row justify-content-between align-items-center">
+              <div class="col-sm-3 my-1">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">Age</div>
+                  </div>
+                  <input type="number" class="form-control" name="student_age">
+                </div>
+              </div>
+              <div class="col-sm-9 my-1">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">Email</div>
+                  </div>
+                  <input type="number" class="form-control" name="student_email" placeholder="jondoe@gmail.com">
+                </div>
+              </div>
+            </div>
+          </form>
   </div>
+
+
 
   <div class="container">
 
-    <table class="table table-bordered">
+    <table class="table table-bordered table-striped table-dark">
       <tr>
         <th>Name</th>
         <th>Age</th>
@@ -70,7 +157,7 @@ if (isset($_POST["submit"])) {
           echo "<td>" . $row["name"] . "</td>";
           echo "<td>" . $row["age"] . "</td>";
           echo "<td>" . $row["email"] . "</td>";
-          echo "<td><a class='btn btn-primary' href='view_edit.php?id=" . $row["s_id"] . "'>Edit</a>
+          echo "<td><a class='btn btn-warning' href='view_edit.php?id=" . $row["s_id"] . "'>Update</a>
         <a class='btn btn-danger' href='delete.php?id=" . $row["s_id"] . "'> Delete</a></td>";
           echo "</tr>";
         }
